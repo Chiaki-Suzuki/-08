f1ef6e6a95369e1d4b45e74ffeba0b8fcff72a1e
Chiaki-Suzuki <c.suzuki.eba@gmail.com>
2022-09-09 11:19:04 +0900

    商品絞り込み

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ let app = new Vue({
   el: '#app',
   data: {
     products: [],
-    kindSort: 'all'
+    kind: 'all'
   },
   created: async function () {
     // JSON読み込み
@@ -13,6 +13,34 @@ let app = new Vue({
   filters: {
     number_format: function (val) {
       return parseInt(val).toLocaleString();
+    }
+  },
+  computed: {
+    /*-------------------------
+      商品絞り込み
+    -------------------------*/
+    kindSort: function () {
+      let newItem = [];
+
+      for (i = 0; i < this.products.length; i++) {
+        let isShow = true
+        // ソフトコーラル
+        if (this.kind === 'soft' && this.products[i].kind !== 'soft') {
+          isShow = false;
+        }
+        // LPS
+        else if (this.kind === 'lps' && this.products[i].kind !== 'lps') {
+          isShow = false;
+        }
+        // SPS
+        else if (this.kind === 'sps' && this.products[i].kind !== 'sps') {
+          isShow = false;
+        }
+        if (isShow) {
+          newItem.push(this.products[i])
+        }
+      }
+      return newItem;
     }
   }
 })
