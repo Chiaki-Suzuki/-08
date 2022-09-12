4630ea21f51a908a7343bdd983f25edbee73806c
Chiaki-Suzuki <c.suzuki.eba@gmail.com>
2022-09-12 14:02:37 +0900

    ページネーション機能追加

--- js/main.js
+++ js/main.js
@@ -2,17 +2,23 @@ let app = new Vue({
   el: '#app',
   data: {
     products: [],
+    pageItems: [],
     kind: 'all',
     count: 8,
     totalStep: '',
     currentStep: 1,
-    isPage: false
+    pagenum: 'pagenum',
+    isPage: null
   },
   created: async function () {
     // JSON読み込み
     const res = await fetch('../実務課題08/js/item.json');
     const items = await res.json();
     this.products = items;
+
+    // 商品一覧表示
+    this.kindSort();
+    this.addClass(1);
   },
   filters: {
     number_format: function (val) {
@@ -23,39 +29,29 @@ let app = new Vue({
     /*-------------------------
       ページネーション
     -------------------------*/
-    currentPage: function (id) {
-      let pageItems = [];
-      let indexStart = id * this.count - this.count;
-      let indexEnd = id * this.count - 1;
-      // 8アイテムを配列に格納
-      for (i = indexStart; i <= indexEnd; i++){
-        pageItems.push(this.products[i])
-      }
-      if (typeof pageItems[0] !== 'undefined') {
-        return pageItems;
-      }
-      return;
-    },
     currentPage2: function (id, newItem) {
-      let pageItems = [];
-      let indexStart = id * this.count - this.count;
-      let indexEnd = id * this.count - 1;
       // 8アイテムを配列に格納
-      for (i = indexStart; i <= indexEnd; i++){
-        pageItems.push(newItem[i])
-      }
-      if (typeof pageItems[0] !== 'undefined') {
-        return pageItems;
-      }
-      return;
+      let indexStart = id * this.count - this.count;
+      let pageItems = newItem.splice(indexStart, this.count);
+      return pageItems;
     },
-    /*-------------------------
-      商品絞り込み
-    -------------------------*/
+    // ページネーション
+    pagenation: function (id) {
+      this.kindSort(id);
+    },
+    // 現在のページにクラス付与
+    addClass: function (id) {
+      if (this.isPage === id) {
+        this.isPage = null;
+      } else {
+        this.isPage = id;
+      }
+    },
     kindSort: function (id) {
       console.log(id)
+      // 商品絞り込み
       let newItem = [];
-  
+
       for (i = 0; i < this.products.length; i++) {
         let isShow = true
         // ソフトコーラル
@@ -74,15 +70,23 @@ let app = new Vue({
           newItem.push(this.products[i])
         }
       }
-  
+
+
       // ページ数を定義
       this.totalStep = Math.ceil(newItem.length / this.count);
+      console.log(newItem)
 
-      if (typeof id === 'undefined') {
-        return newItem;
-      } else {
-        console.log(this.currentPage2(id, newItem))
-        return this.currentPage2(id, newItem);
+      // ページ読み込み時
+      if (typeof id === 'undefined' && typeof newItem[0] !== 'undefined') {
+        this.pageItems = this.currentPage2(1, newItem);
+      }
+      // ページネーションクリック時
+      else if (typeof id !== 'undefined' && typeof newItem[0] !== 'undefined') {
+
+        // クラス付与
+        this.addClass(id);
+
+        this.pageItems = this.currentPage2(id, newItem);
       }
     }
   },
